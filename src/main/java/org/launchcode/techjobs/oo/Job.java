81209e4549e55026e8c592348cf380484572716f
torijameson <vtjameson@gmail.com>
2022-02-19 23:13:07 -0600

    First commit of correct pull. Commiting in case there is an error with incorrect pull

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -13,13 +13,77 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
-    //  other five fields. The second constructor should also call the first in order to initialize
-    //  the 'id' field.
+
+    //***** copied from first file- 2 constructors
+    // Step 1: constructor for a unique ID
+    public Job () {
+        id=nextId;
+        nextId++;
+    }
+    //Step 2: second constructor calling the first, and initalizng other fields
+
+
+    public Job (String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency){
+        this();
+        this.name=name;
+        this.employer=employer;
+        this.location=location;
+        this.positionType=positionType;
+        this.coreCompetency=coreCompetency;
+    }
+
 
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+    //******** Done. Custom equals and hashcode methods . equal when id fields match (is this here?)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
 
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+    // ******* Done:  Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public Employer getEmployer() {
+        return employer;
+    }
+    public Location getLocation() {
+        return location;
+    }
+    public PositionType getPositionType() {
+        return positionType;
+    }
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
 }
---
+++ src/main/java/org/launchcode/techjobs/oo/Tests/JobTest.java
@@ -0,0 +1,25 @@
+package org.launchcode.techjobs.oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JobTest {
+    @Before
+    public void createJobObject() {
+        //JobOne = new Job("");
+       // JobTwo = new Job(JobOne++);
+    }
+
+    @Test
+    public void testSettingJobId(){
+
+       // assertEquals(expected, actual, optional_delta);
+
+      //  assertFalse(condition);
+
+      //  assertTrue(condition);
+
+       // assertNotNull(object);
+
+    }
+}
